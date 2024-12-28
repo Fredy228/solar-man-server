d6834572702322cce39793da775470443785a452
Fredy228 <smagrovich58@gmail.com>
2024-12-28 22:41:27 +0200

    Fix static

--- index.js
+++ index.js
@@ -1,43 +1,43 @@
-const express = require('express');
-const logger = require('morgan');
-const cors = require('cors');
-const path = require('path');
-const sendEmailRouter = require('./routes/sendEmailRoute');
-const usersRouter = require('./routes/usersRoute');
-const portfolioRouter = require('./routes/portfolioRoute');
-const storeSetsRouter = require('./routes/storeSetsRoutes');
-const storeComponentsRouter = require('./routes/storeComponentsRoutes');
+const express = require("express");
+const logger = require("morgan");
+const cors = require("cors");
+const path = require("path");
+const sendEmailRouter = require("./routes/sendEmailRoute");
+const usersRouter = require("./routes/usersRoute");
+const portfolioRouter = require("./routes/portfolioRoute");
+const storeSetsRouter = require("./routes/storeSetsRoutes");
+const storeComponentsRouter = require("./routes/storeComponentsRoutes");
 
 const app = express();
 
-const formatsLogger = app.get('env') === 'development' ? 'dev' : 'short';
+const formatsLogger = app.get("env") === "development" ? "dev" : "short";
 
 app.use(logger(formatsLogger));
 app.use(cors());
 app.use(express.json());
-app.use(express.static('build'));
-app.use(express.static('static'));
+app.use(express.static("build"));
+app.use("/api", express.static("static"));
 
-app.use('/api/phone-send', sendEmailRouter);
-app.use('/api/admin', usersRouter);
-app.use('/api/admin/portfolio', portfolioRouter);
-app.use('/api/admin/store-sets', storeSetsRouter);
-app.use('/api/admin/store-components', storeComponentsRouter);
+app.use("/api/phone-send", sendEmailRouter);
+app.use("/api/admin", usersRouter);
+app.use("/api/admin/portfolio", portfolioRouter);
+app.use("/api/admin/store-sets", storeSetsRouter);
+app.use("/api/admin/store-components", storeComponentsRouter);
 
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
-});
+// app.get("*", (req, res) => {
+//   res.sendFile(path.join(__dirname, "build", "index.html"));
+// });
 
 app.use((req, res) => {
-  res.status(404).json({ message: 'Not found' });
+  res.status(404).json({ message: "Not found" });
 });
 
 app.use((err, req, res, next) => {
-  const { status = 500, message = 'Server error' } = err;
+  const { status = 500, message = "Server error" } = err;
   res.status(status).json({ message });
 });
 
-const PORT = process.env.NODE_ENV === 'development' ? 3001 : 5000;
+const PORT = process.env.NODE_ENV === "development" ? 3001 : 5000;
 
 app.listen(PORT, () => {
   console.log(`Server started on port ${PORT}`);
